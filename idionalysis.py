85476662cae45781c541f0bbed1e683bf612195a
Aardbei <megaloler9000@gmail.com>
2019-03-01 15:03:31 -0600

    graphs

--- idionalysis.py
+++ idionalysis.py
@@ -2,20 +2,46 @@
 
 import sys
 
+GRAPH_BINS = 50
+ROUND_DIGITS = 2
+
+def divide(n, m):
+    ''' integer divide n by m into explicit divisions
+
+    return each division as a list such that the sum equals m
+    '''
+    # keep track of each subsequent division
+    divisions = list()
+    while m:
+        division = n // m
+        divisions.append(division)
+        n -= division
+        m -= 1
+    return divisions
+
 class Analysis:
     ''' a report of data from analysis '''
-    def __init__(self, text):
-        ''' do the analysis and store the data '''
 
+    @staticmethod
+    def from_text(text):
+        ''' create an analysis from a text '''
+        analysis = Analysis()
+        analysis.analyse(text)
+        return analysis
+
+    def __init__(self, words=None, initial_encounters=None, occurences=None):
+        self.words = words
+        self.initial_encounters = initial_encounters
+        self.occurences = occurences
+
+    def analyse(self, text):
+        ''' do the analysis and store the data '''
         # get the words in the text
         self.words = text.split()
-
         # to be list of bools corresponding to which words are first encounters
         self.initial_encounters = list()
-
         # keep track of unique words and their number of occurences
         self.occurences = dict()
-
         # loop through each word
         for word in self.words:
             # record word count
@@ -33,6 +59,30 @@ class Analysis:
         # return whether it was a first encounter
         return initial_encounter
 
+    def divide(self, chunks):
+        ''' divide the analysis into smaller chunks '''
+        # i question this method of dividing..
+        # it feels like an abuse of this class lol
+        # anyway
+        # get the amount of words for each chunk
+        chunk_lengths = divide(self.length, chunks)
+        # get the chunks
+        chunks = list()
+        # cycle through each chunk length and collect that chunk
+        start = 0
+        for length in chunk_lengths:
+            end = start + length
+            initial_encounters = self.initial_encounters[start:end]
+            chunk = Analysis(self.words, initial_encounters)
+            chunks.append(chunk)
+            start += length
+        return chunks
+
+    def graph(self, chunks):
+        ''' return the concentration of how many initial words each divided chunk has '''
+        chunks = self.divide(chunks)
+        return list(map(lambda chunk: chunk.unique / chunk.length, chunks))
+
     @property
     def length(self):
         ''' return how many words in the text '''
@@ -41,19 +91,30 @@ class Analysis:
     @property
     def unique(self):
         ''' return how many unique words in the text '''
-        return len(self.occurences)
+        return len(self.occurences) if self.occurences else self.initial_encounters.count(True)
+
+    @property
+    def ratio(self):
+        ''' return the ratio of unique to total occurences '''
+        return self.unique / self.length
 
     def __str__(self):
         ''' make a nice report '''
+        def prettify(value):
+            ''' stringify a value as a rounded percentage '''
+            return f'{round(value * 100, ROUND_DIGITS)}%'
         lines = list()
         lines.append(f'total words:    {self.length}')
         lines.append(f'unique words:   {self.unique}')
-        lines.append(f'unique / total: {round(self.unique / self.length * 100, 2)}%')
+        lines.append(f'unique / total: {prettify(self.ratio)}')
+        graph = self.graph(GRAPH_BINS)
+        graph_str = ' + '.join(map(prettify, graph))
+        lines.append(f'{graph_str} = {prettify(sum(graph))}')
         return '\n'.join(lines)
 
 def analyse_file(filename):
     with open(filename) as f:
-        return Analysis(f.read())
+        return Analysis.from_text(f.read())
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
